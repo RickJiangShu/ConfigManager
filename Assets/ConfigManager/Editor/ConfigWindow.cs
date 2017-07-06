362c5ac0f2d893a143297738164056bc90cc94d8
Rick <rickjiangshu@gmail.com>
2017-07-06 18:04:17 +0800

    update

---
+++ Assets/ConfigManager/Editor/ConfigWindow.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author:  Rick
+ * Create:  2017/7/6 18:01:59
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// ConfigManager窗口
+/// </summary>
+public class ConfigWindow : ScriptableObject
+{
+    [MenuItem("Windows/ConfigManager")]
+    static void DoIt()
+    {
+        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+    }
+}

ca87cf2d1e4f5ecc99fa379c64cb3a7e140455b8
Rick <rickjiangshu@gmail.com>
2017-07-10 22:16:44 +0800

    clear

---
+++ Editor/Generator/SerializableSetGenerator.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Author:  Rick
+ * Create:  2017/7/10 15:04:49
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+namespace ConfigManagerEditor
+{
+    using System;
+    using System.Collections.Generic;
+    /// <summary>
+    /// 序列化集合 生产
+    /// </summary>
+    public class SerializableSetGenerator
+    {
+        private const string templete =
+@"[System.Serializable]
+public class SerializableSet : UnityEngine.ScriptableObject
+{
+/*DeclareConfigs*/
+}
+";
+        private const string templete2 = 
+@"    public /*ConfigName*/[] /*SourceName*/s;
+";
+
+        public static void Generate(List<Source> sources, string outputFolder)
+        {
+            string outputPath = outputFolder + "/SerializableSet.cs";
+            string content = templete;
+
+            string declareConfigs = "";
+            foreach (Source src in sources)
+            {
+                string declare = templete2;
+                declare = declare.Replace("/*ConfigName*/", src.configName);
+                declare = declare.Replace("/*SourceName*/", src.sourceName);
+                declareConfigs += declare;
+            }
+
+            content = content.Replace("/*DeclareConfigs*/", declareConfigs);
+            ConfigTools.WriteFile(outputPath, content);
+        }
+    }
+}
---
+++ Editor/Source.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Author:  Rick
+ * Create:  2017/7/10 13:49:46
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+namespace ConfigManagerEditor
+{
+    /// <summary>
+    /// 源数据
+    /// </summary>
+    public class Source
+    {
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content;
+
+        /// <summary>
+        /// 源文件的文件名
+        /// </summary>
+        public string sourceName;
+
+        /// <summary>
+        /// 配置的文件名
+        /// </summary>
+        public string configName;
+
+        /// <summary>
+        /// 解析出来的矩阵
+        /// </summary>
+        public string[,] matrix;
+
+        /// <summary>
+        /// 行
+        /// </summary>
+        public int row;
+
+        /// <summary>
+        /// 列
+        /// </summary>
+        public int column;
+    }
+}
